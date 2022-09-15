90628f7d4abef4c935aa56c054aba457c5edd55c
pardedeputra27 <pardedeputra27@gmail.com>
2022-09-15 10:22:51 +0700

    mempercanntik tampilan form

--- lib/main2.dart
+++ lib/main2.dart
@@ -74,7 +74,7 @@ class _DetailAttendanceState extends State<DetailAttendance> {
               thickness: 3,
             ),
             SizedBox(
-              height: 647,
+              height: 580,
               child: FutureBuilder(
                 future: getAttendanceDataSource(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
